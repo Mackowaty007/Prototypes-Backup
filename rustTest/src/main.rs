63eae7b26d050df25b4f519186633f3557a26dd9
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2023-01-05 23:02:03 +0100

    Add files via upload

--- rustTest/src/main.rs
+++ rustTest/src/main.rs
@@ -1,6 +1,11 @@
 #[macro_use]
 extern crate glium;
 
+use std::io::Cursor;
+
+const PI:f32 = 3.14159265359;
+
+
 fn main() {
     #[allow(unused_imports)]
     use glium::{glutin, Surface};
@@ -10,49 +15,56 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    let image = image::load(Cursor::new(&include_bytes!("../textures/Stone_Pavement_DIFF.png")),
+                            image::ImageFormat::Png).unwrap().to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
 
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        tex_coords: [f32; 2],
     }
 
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, tex_coords);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let mut t: f32 = -0.5;
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
 
     let vertex_shader_src = r#"
         #version 140
-
         in vec2 position;
-        uniform float t;
-
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+        uniform mat4 matrix;
         void main() {
-            vec2 pos = position;
-            pos.x += t;
-            gl_Position = vec4(pos, 0.0, 1.0);
+            v_tex_coords = tex_coords;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
+        in vec2 v_tex_coords;
         out vec4 color;
+        uniform sampler2D tex;
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = texture(tex, v_tex_coords);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    event_loop.run(move |ev, _, control_flow| {
-        match ev {
+    let mut t = -0.5;
+    event_loop.run(move |event, _, control_flow| {
+
+        match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
@@ -60,26 +72,39 @@ fn main() {
                 },
                 _ => return,
             },
-            _ => (),
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
         }
 
         let next_frame_time = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        t += 0.0002;
-        if t>0.5{
-            t = -0.5;
+        // we update `t`
+        t += 0.02;
+        if t > 2.0*PI {
+            t = 0.0;
         }
+
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t},
-            &Default::default()).unwrap();
+
+        let uniforms = uniform! {
+            matrix: [
+                [t.sin(), t.cos(), 0.0, 0.0],
+                [-t.cos(), t.sin(), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ],
+            tex: &texture,
+        };
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                    &Default::default()).unwrap();
         target.finish().unwrap();
-
-        
-
-        
-
     });
 }
