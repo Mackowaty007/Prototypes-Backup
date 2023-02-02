9d343ec763c4aa69de0e356d9492ac7216c7c4bb
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2023-02-02 23:42:17 +0100

    Add files via upload

--- ManelbrotShader/src/main.rs
+++ ManelbrotShader/src/main.rs
@@ -12,12 +12,16 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    //time
+    let mut t = -0.5;
+
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        t: f32,
     }
 
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, t);
 
     let vertex1 = Vertex { position: [-1.0, -1.0] };
     let vertex2 = Vertex { position: [-1.0,  1.0] };
@@ -33,7 +37,8 @@ fn main() {
     let vertex_shader_src = r#"
         #version 140
         in vec2 position;
-        out vec2 my_position;      // our new attribute
+        in float t;
+        out vec2 my_position;
         uniform mat4 matrix;
         void main() {
             my_position = position;     // we need to set the value of each `out` variable.
@@ -70,7 +75,6 @@ fn main() {
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let mut t = -0.5;
     event_loop.run(move |event, _, control_flow| {
 
         match event {
@@ -86,16 +90,10 @@ fn main() {
                 glutin::event::StartCause::Init => (),
                 _ => return,
             },
-
-            /* The code to get the mouse position (And print it to the console) */
-            //glutin::event::WindowEvent::CursorMoved { position, .. } => {
-            //    println!("Mouse position: {:?}x{:?}", position.x as u16, position.y as u16);
-            //}
             _ => return,
         }
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         // we update `t`
@@ -116,10 +114,8 @@ fn main() {
             ]
         };
 
-        target.draw(&vertex_buffer1, &indices, &program, &uniforms,
-            &Default::default()).unwrap();
-        target.draw(&vertex_buffer2, &indices, &program, &uniforms,
-            &Default::default()).unwrap();
+        target.draw(&vertex_buffer1, &indices, &program, &uniforms,&Default::default()).unwrap();
+        target.draw(&vertex_buffer2, &indices, &program, &uniforms,&Default::default()).unwrap();
         target.finish().unwrap();
     });
 }
