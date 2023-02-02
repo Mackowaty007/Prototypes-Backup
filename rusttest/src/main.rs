4d951fab39c47a24387c3cbb15f12e651042d07a
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2023-02-02 23:43:06 +0100

    Add files via upload

--- rusttest/src/main.rs
+++ rusttest/src/main.rs
@@ -1,81 +1,174 @@
-//! Basic hello world example.
+#[macro_use]
+extern crate glium;
 
-use ggez::{event, graphics, Context, GameResult};
-use std::{env, path};
+#[allow(unused_imports)]
+use glium::{glutin, Surface};
+use glium::index::PrimitiveType;
 
-// First we make a structure to contain the game's state
-struct MainState {
-    frames: usize,
-}
+fn main() {
+    // building the display, ie. the main object
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new().with_title("Tetris");
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
-        //ctx.gfx.add_font(
-        //    "LiberationMono",
-        //    graphics::FontData::from_path(ctx, "/LiberationMono-Regular.ttf")?,
-        //);
-
-        let s = MainState { frames: 0 };
-        Ok(s)
-    }
-}
-
-// Then we implement the `ggez:event::EventHandler` trait on it, which
-// requires callbacks for updating and drawing the game state each frame.
-//
-// The `EventHandler` trait also contains callbacks for event handling
-// that you can override if you wish, but the defaults are fine.
-impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        Ok(())
+    // building the vertex buffer, which contains all the vertices that we will draw
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+        color: [f32; 3],
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas =
-            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+    implement_vertex!(Vertex, position, color);
 
-        // Text is drawn from the top-left corner.
-        let offset = self.frames as f32 / 10.0;
-        let dest_point = ggez::glam::Vec2::new(offset, offset);
-        canvas.draw(
-            graphics::Text::new("Hello, world!")
-                .set_scale(48.),
-            dest_point,
-        );
+    let vertex_buffer = 
+        glium::VertexBuffer::new(&display,
+                &[Vertex {
+                   position: [-0.5, -0.5],
+                   color: [0.0, 1.0, 0.0],
+               },
+               Vertex {
+                   position: [0.0, 0.5],
+                   color: [0.0, 0.0, 1.0],
+               },
+               Vertex {
+                   position: [0.5, -0.5],
+                   color: [1.0, 0.0, 0.0],
+               }])
+            .unwrap();
 
-        canvas.finish(ctx)?;
+    // building the index buffer
+    let index_buffer =
+        glium::IndexBuffer::new(&display, PrimitiveType::TrianglesList, &[0u16, 1, 2]).unwrap();
 
-        self.frames += 1;
-        if (self.frames % 100) == 0 {
-            println!("FPS: {}", ctx.time.fps());
-        }
+    // compiling shaders and linking them together
+    let program = program!(&display,
+        140 => {
+            vertex: "
+                #version 140
 
-        Ok(())
-    }
-}
+                uniform mat4 matrix;
 
-// Now our main function, which does three things:
-//
-// * First, create a new `ggez::ContextBuilder`
-// object which contains configuration info on things such
-// as screen resolution and window title.
-// * Second, create a `ggez::game::Game` object which will
-// do the work of creating our MainState and running our game.
-// * Then, just call `game.run()` which runs the `Game` mainloop.
-pub fn main() -> GameResult {
-    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
-    // so that ggez will look in our cargo project directory for files.
-    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
-        let mut path = path::PathBuf::from(manifest_dir);
-        path.push("resources");
-        path
-    } else {
-        path::PathBuf::from("./resources")
+                in vec2 position;
+                in vec3 color;
+
+                out vec3 vColor;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 140
+                in vec3 vColor;
+                out vec4 f_color;
+
+                void main() {
+                    f_color = vec4(vColor, 1.0);
+                }
+            "
+        },
+
+        110 => {
+            vertex: "
+                #version 110
+
+                uniform mat4 matrix;
+
+                attribute vec2 position;
+                attribute vec3 color;
+
+                varying vec3 vColor;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 110
+                varying vec3 vColor;
+
+                void main() {
+                    gl_FragColor = vec4(vColor, 1.0);
+                }
+            ",
+        },
+    )
+        .unwrap();
+
+    // drawing once
+
+    // building the uniforms
+    let uniforms = uniform! {
+        matrix: [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32]
+        ]
     };
 
-    let cb = ggez::ContextBuilder::new("helloworld", "ggez").add_resource_path(resource_dir);
-    let (mut ctx, event_loop) = cb.build()?;
+    event_loop.run(move |event, _, control_flow| {
+        // React to events
+        use glium::glutin::event::{Event, WindowEvent, ElementState, VirtualKeyCode};
 
-    let state = MainState::new(&mut ctx)?;
-    event::run(ctx, event_loop, state)
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if let ElementState::Pressed = input.state {
+                        if let Some(VirtualKeyCode::S) = input.virtual_keycode {
+                            println!("test");
+                        }
+                    }
+                },
+                _ => return,
+            },
+            Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
+        }
+
+        let vertex_buffer = 
+        glium::VertexBuffer::new(&display,
+                &[Vertex {
+                   position: [-0.5, -0.5],
+                   color: [0.0, 1.0, 0.0],
+               },
+               Vertex {
+                   position: [0.0, 0.5],
+                   color: [0.0, 0.0, 1.0],
+               },
+               Vertex {
+                   position: [0.5, -0.5],
+                   color: [1.0, 0.0, 0.0],
+               }])
+            .unwrap();
+
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target.draw(&vertex_buffer,
+                  &index_buffer,
+                  &program,
+                  &uniforms,
+                  &Default::default())
+            .unwrap();
+        target.finish().unwrap();
+    });
 }
